bbfbcb6716938e90884c58009b078c80d64e8c7b
amccartney <allison.n.mccartney@gmail.com>
2015-09-11 09:23:41 -0400

    adding new template work

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask
+from flask import abort
+from flask import render_template
+from helpers import hp_data
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    template = 'index.html'
+    return render_template(template)
+
+@app.route('/overview/')
+def overview_data():
+    template = 'company_overview.html'
+    object_list = hp_data.load_data()
+    products_list = hp_data.dollars_by_product_service_code(object_list)
+    recipient_city_list = hp_data.contracts_by_city(object_list)
+    return render_template(template, object_list=object_list, products_list=products_list, recipient_city_list=recipient_city_list)
+
+@app.route('/product/<product_id>/')
+def contract_list(product_id):
+    template = 'list.html'
+    object_list = hp_data.load_data()
+    products_list = hp_data.dollars_by_product_service_code(object_list)
+    for p in products_list:
+        if p[4] == product_id:
+            object_list = [o for o in object_list if p[0] == o["ProductorServiceCode"]]
+            return render_template(template, object_list=object_list)
+    abort(404)
+
+
+@app.route('/city/<city_id>/')
+def list(city_id):
+    template = 'geo-list.html'
+    object_list = hp_data.load_data()
+    cities_list = hp_data.contracts_by_city(object_list)
+    for c in cities_list:
+        if c[5] == city_id:
+            object_list = [o for o in object_list if c[5] == o["RecipientCity"]]
+            return render_template(template, object_list=object_list)
+    abort(404)
+
+
+@app.route('/product/<product_id>/<contract_id>/')
+def product_contract(product_id, contract_id):
+    template = 'contract.html'
+    object_list = hp_data.load_data()
+    record = [o for o in object_list if o['record_count'] == contract_id]
+    return render_template(template, object=record)
+
+
+@app.route('/city/<city_id>/<contract_id>/')
+def city_contract(city_id, contract_id):
+    template = 'contract.html'
+    object_list = hp_data.load_data()
+    record = [o for o in object_list if o['record_count'] == contract_id]
+    return render_template(template, object=record)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
---
+++ helpers/hp_data.py
@@ -0,0 +1,93 @@
+import json
+import operator
+import os
+import requests
+
+DATA_PATH = os.path.join(os.path.dirname(__file__), '../static/hp-data-2015.json')
+def load_data():
+    with open(DATA_PATH) as f:
+        d = json.loads(f.read())
+    return d['usaspendingSearchResults']['result']['doc']
+
+
+def contracts_by_agency(hpdata):
+    agencies = {}
+    for h in hpdata:
+        agency = h['AgencyID']
+        contract_amount = float(h['DollarsObligated'])
+        if agency in agencies:
+            agencies[agency] += contract_amount
+        else:
+            agencies[agency] = contract_amount
+    return agencies
+
+
+###################################################
+# CONTRACTS BY RECIPIENT CITY
+
+def contracts_by_city(hpdata):
+    recipient_city = []
+    cities = []
+    for h in hpdata:
+        city_state = str(h['RecipientCity'] + ", " + h['RecipientState'])
+        ident = h['RecipientCity']
+        contract_amount = int(float(h['DollarsObligated']))
+        if city_state not in cities:
+            recipient_city.append([city_state, contract_amount, 1, 'x', 'y', ident])
+            cities.append(city_state)
+        else:
+            for r in recipient_city:
+                if r[0] == city_state:
+                    r[1] += contract_amount
+                    r[2] += 1
+
+    recipient_city = sorted(recipient_city, key=operator.itemgetter(1), reverse=True)
+    
+
+    # GEOCODING
+    # maps_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
+    # for r in recipient_city:
+    #     atts = {'address': r[0], 'key': 'AIzaSyDdBsQKgsXLrHAil_feh5RK_g5lyxbRmAU'}
+    #     resp = requests.get(maps_url, params = atts)
+    #     data = resp.json()
+    #     r[3] = data['results'][0]['geometry']['location']['lat']
+    #     r[4] = data['results'][0]['geometry']['location']['lng']
+
+    # return recipient_city
+
+
+###################################################
+
+
+# DOLLARS OBLIGATED BY FISCAL YEAR
+
+###################################################
+# DOLLARS OBLIGATED BY PRODUCTS OR SERVICE CODE
+def dollars_by_product_service_code(hpdata):
+    products_code = []
+    codes = []
+    for h in hpdata:
+        code = h['ProductorServiceCode']
+        contract_amount = int(float(h['DollarsObligated']))
+        if code not in codes:
+            products_code.append([code, contract_amount, 1, 'width', 'id'])
+            codes.append(code)
+        else:
+            for p in products_code:
+                if p[0] == code:
+                    p[1] += contract_amount
+                    p[2] += 1
+
+    products_code = sorted(products_code, key=operator.itemgetter(1), reverse=True)
+
+    # TO GET BAR WIDTH IN CHART
+    unit = products_code[0][1] / 100
+
+    for p in products_code:
+        width = p[1] / unit
+        p[3] = width * 2
+        ident = p[0].split(':')
+        ident = ident[0]
+        p[4] = ident
+
+    return products_code
