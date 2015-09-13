6ea6f8c25cc395cd487883572efd56bb27b6338e
amccartney <allison.n.mccartney@gmail.com>
2015-09-13 14:04:10 -0400

    more template work

--- app.py
+++ app.py
@@ -12,13 +12,56 @@ def index():
     template = 'index.html'
     return render_template(template)
 
-@app.route('/overview/')
+@app.route('/company-overview/')
 def overview_data():
     template = 'company_overview.html'
     object_list = hp_data.load_data()
     products_list = hp_data.dollars_by_product_service_code(object_list)
     recipient_city_list = hp_data.contracts_by_city(object_list)
     return render_template(template, object_list=object_list, products_list=products_list, recipient_city_list=recipient_city_list)
+
+@app.route('/contract-list/')
+def list_view():
+    template = 'list_view.html'
+    object_list = hp_data.load_data()
+    products_list = hp_data.dollars_by_product_service_code(object_list)
+    recipient_city_list = hp_data.contracts_by_city(object_list)
+    return render_template(template, object_list=object_list, products_list=products_list, recipient_city_list=recipient_city_list)
+
+@app.route('/contract-detail/')
+def contract_view():
+    template = 'contract_view.html'
+    object_list = hp_data.load_data()
+    products_list = hp_data.dollars_by_product_service_code(object_list)
+    recipient_city_list = hp_data.contracts_by_city(object_list)
+    return render_template(template, object_list=object_list, products_list=products_list, recipient_city_list=recipient_city_list)
+
+@app.route('/glossary/')
+def glossary():
+    template = 'glossary.html'
+    return render_template(template)
+
+@app.route('/about-the-data/')
+def about_the_data():
+    template = 'about-the-data.html'
+    return render_template(template)
+
+@app.route('/search-tips/')
+def search_tips():
+    template = 'search-tips.html'
+    return render_template(template)       
+
+
+
+
+
+
+
+
+
+
+
+# OLD PAGE VIEWS
 
 @app.route('/product/<product_id>/')
 def contract_list(product_id):
